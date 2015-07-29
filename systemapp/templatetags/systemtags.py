131996f4b11854fc0cf5e5177da6e96d5ab0433f
alejandrojnm <alejandrojnm@gmail.com>
2015-07-29 02:12:57 -0400

    Update, Agreegate font Roboto, fix style and add filter humanduration for convert millisec to human read time, fix interface in listHost.html

---
+++ systemapp/templatetags/systemtags.py
@@ -0,0 +1,38 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+
+def humanduration(value):
+    """
+    Filter milisec to human time
+    :param value:
+    :return:
+    """
+
+    time = int(value)
+
+    # sec
+    if time / 1000 < 60:
+        return "%i Sec" % (time / 1000)
+
+    # min
+    if time / 1000 < 60:
+        return "%i Min" % (time / 1000 / 60)
+
+    # hours
+    if time / 1000 / 60 > 60:
+        return "%i hours" % (time / 1000 / 3600)
+
+
+def js(obj):
+    """
+    Cleaner js from DB for Filter CamanJS
+    :param obj:
+    :return:
+    """
+    return mark_safe(obj)
+
+
+register = template.Library()
+register.filter('humanDuration', humanduration)
+register.filter('js', js)
